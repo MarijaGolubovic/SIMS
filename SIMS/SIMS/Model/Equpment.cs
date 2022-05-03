bd0a623b98e05eb7c37d91e2d13844e71b8c4c66
Marija Golubovic <marijagolubovic16@gmail.com>
2022-05-03 14:15:57 +0200

    Create RoomEquipment storage, controller and service

---
+++ SIMS/SIMS/Model/Equpment.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS.Model
+{
+    class Equpment: Serialization.Serializable
+    {
+        public String Name { get; set; }
+        public int Quantity{ get; set; }
+
+        public Equpment(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+
+        public Equpment()
+        {
+        }
+
+        public string[] toCSV()
+        {
+            string[] csvValues = {
+                Name,
+                Quantity.ToString()
+
+            };
+            return csvValues;
+        }
+
+        public void fromCSV(string[] values)
+        {
+            Name = values[0];
+            Quantity = int.Parse(values[1]);
+        }
+
+        String _Name;
+        int _Quantity;
+
+        public static explicit operator Equpment(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SIMS/SIMS/Model/RoomEqupment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS.Model
+{
+    class RoomEqupment : Serialization.Serializable
+    {
+
+        private readonly Controller.EquipmentController equipmentController = new Controller.EquipmentController();
+
+        public String RoomId { get; set; }
+        public List<Equpment> roomEquipment { get; set; }
+
+        String _RoomId;
+        List<Equpment> _RoomEquipment { get; set; }
+
+        public string[] toCSV()
+        {
+            string[] csvValues = {RoomId};
+            int i = 1;
+            foreach (Equpment equipment in roomEquipment) {
+                csvValues[i] = equipment.ToString();
+                i++;
+            }
+
+            return csvValues;
+        }
+
+        public void fromCSV(string[] values)
+        {
+            RoomId = values[0];
+            
+            if (values == null)
+                return;
+
+            int i;
+            for(i = 1; i < values.Length; i++){
+                roomEquipment.Add(((Equpment)values[i]));
+            }
+            
+        }
+
+        public RoomEqupment(string roomId, List<Equpment> roomEquipment)
+        {
+            RoomId = roomId;
+            this.roomEquipment = roomEquipment;
+        }
+
+        public RoomEqupment()
+        {
+        }
+    }
+}
